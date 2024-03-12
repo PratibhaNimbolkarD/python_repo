e45e88ec4b52ef95d7d9a599e79dc0eced1c1ee9
pratibha nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-03-12 17:58:51 +0530

    loggers updated

--- src/mutations/driver.py
+++ src/mutations/driver.py
@@ -1,6 +1,8 @@
+import  logging
 from util import mutate_string
+logging.basicConfig(level=logging.INFO , format= '%(message)s')
 if __name__ == '__main__':
     s = input("Enter the string : ")
     i, c = input("Enter the index and the letter : ").split()
     s_new = mutate_string(s, int(i), c)
-    print(s_new)
+    logging.info(s_new)

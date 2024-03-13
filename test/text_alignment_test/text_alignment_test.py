a9ece3db4237710788a07e5e5dd49cab465612ea
pratibha nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-03-13 18:39:37 +0530

    test file added for text alignment

---
+++ test/text_alignment_test/text_alignment_test.py
@@ -0,0 +1,30 @@
+import unittest
+from src.text_alignment.util import text_alignment
+class TextAlignmentTest(unittest.TestCase):
+    def test_one(self):
+        self.assertEqual(text_alignment(),'''    H    
+   HHH   
+  HHHHH  
+ HHHHHHH 
+HHHHHHHHH
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHHHHHHHHHHHHHHHHHHHHHH   
+  HHHHHHHHHHHHHHHHHHHHHHHHH   
+  HHHHHHHHHHHHHHHHHHHHHHHHH   
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+                    HHHHHHHHH 
+                     HHHHHHH  
+                      HHHHH   
+                       HHH    
+                        H     
+''')

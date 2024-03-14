aed0f6b814038f9a8d59129f89fba119b44d52b9
pratibha nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-03-14 18:22:38 +0530

    word order

---
+++ src/word_order/util.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+
+def fun():
+    n = int(input("Enter the number of elements: "))
+    l = []
+
+    for i in range(n):
+        l.append(input(f"Enter element {i + 1}: "))
+
+    c = Counter(l)
+    return ' '.join(str(value) for value in c.values())

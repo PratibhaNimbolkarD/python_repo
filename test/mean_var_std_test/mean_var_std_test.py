a52d8a6401637c4dbfce7676038430c805af03e7
pratibha nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-03-14 18:17:49 +0530

    mean var std test

---
+++ test/mean_var_std_test/mean_var_std_test.py
@@ -0,0 +1,34 @@
+import unittest
+
+from src.mean_var_std.util import mean_var_std
+
+
+class MyTestCase(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(mean_var_std(), 1.11803398875)
+
+    ''' 2 2 input
+            1 2
+            3 4
+            1.11803398875 (output)'''
+
+    def test2(self):
+        self.assertEqual(mean_var_std(), 1.224744871392)
+
+    '''     2 4 (input)
+            7 5
+            8 8
+            1.224744871392 (output)'''
+
+    def test3(self):
+        self.assertEqual(mean_var_std(), 2.793842435707)
+
+    ''' 3 6 (input)
+        8 9 
+        3 4
+        1 4
+        2.793842435707 (output)'''
+
+
+if __name__ == '__main__':
+    unittest.main()

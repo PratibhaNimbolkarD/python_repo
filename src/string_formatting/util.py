1c03ba88309d0f7909c0764fe03b124295444516
pratibha nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-03-14 18:21:26 +0530

    string formatting

--- src/string_formatting/util.py
+++ src/string_formatting/util.py
@@ -1,7 +1,12 @@
 import logging
+logging.basicConfig(level=logging.INFO , format='%(message)s')
 
 def string_formatting():
     number = int(input("Enter the number : "))
     length = len("{0:b}".format(number))
+    result=""
     for i in range(1, number + 1):
-         print("{0:{w}d} {0:{w}o} {0:{w}X} {0:{w}b}".format(i, w=length))
+         logging.info("{0:{w}d} {0:{w}o} {0:{w}X} {0:{w}b}".format(i, w=length))
+         a = "{0:{w}d} {0:{w}o} {0:{w}X} {0:{w}b}".format(i, w=length)
+         result += a+'\n'
+    return (result)
